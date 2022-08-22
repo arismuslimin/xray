dc7441e041843c87225d4276d87c33fd593d0878
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-23 05:56:19 +0700

    Update updatedll.sh

--- menu/updatedll.sh
+++ menu/updatedll.sh
@@ -4,11 +4,11 @@
 # download 
 cd /usr/bin
 
-wget -O add-akun "https://raw.githubusercontent.com/adamfuadia/testxray/main/menu/add-akun.sh"
-wget -O updatedll "https://raw.githubusercontent.com/adamfuadia/testxray/main/menu/updatedll.sh"
-wget -O add-akun "https://raw.githubusercontent.com/adamfuadia/testxray/main/menu/add-akun.sh"
-wget -O delete-akun "https://raw.githubusercontent.com/adamfuadia/testxray/main/menu/delete-akun.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/certv2ray.sh"
+wget -O add-akun "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/add-akun.sh"
+wget -O updatedll "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/updatedll.sh"
+wget -O add-akun "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/add-akun.sh"
+wget -O delete-akun "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/delete-akun.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/arismuslimin/xray/main/xray/certv2ray.sh"
 
 #
 chmod +x add-akun
