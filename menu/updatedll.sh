7a837eacaab827dda87db13241dc0c101a17d82b
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-23 17:13:16 +0700

    Update updatedll.sh

--- menu/updatedll.sh
+++ menu/updatedll.sh
@@ -16,17 +16,14 @@ cd /usr/bin
 wget -O xmenu "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/xmenu.sh"
 wget -O add-akun "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/add-akun.sh"
 wget -O updatedll "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/updatedll.sh"
-wget -O add-akun "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/add-akun.sh"
 wget -O delete-akun "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/delete-akun.sh"
 wget -O certv2ray "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/certv2ray.sh"
 wget -O restart-xray "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/restart-xray.sh"
-wget -O xmenu "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/xmenu.sh"
 wget -O auto-pointing "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/auto-pointing.sh"
 wget -O cek-port "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/cek-port.sh"
 wget -O backup "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/backup.sh"
 wget -O autobackup "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/autobackup.sh"
 wget -O restore "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/restore.sh"
-wget -O xmenu "https://raw.githubusercontent.com/arismuslimin/xray/main/menu/xmenu.sh"
 
 #
 chmod +x /usr/bin/updatedll
@@ -35,7 +32,6 @@ chmod +x xmenu
 chmod +x add-akun
 chmod +x delete-akun
 chmod +x updatedll
-chmod +x add-akun
 chmod +x certv2ray
 chmod +x restart-xray
 chmod +x auto-pointing
